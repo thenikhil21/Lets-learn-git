05fa2edc9ad36e4e730a15449c8660039c99cbf4
Nikhil Gautam <nikhilgatuam5254@gmail.com>
2026-01-02 17:00:43 +0530

    implemented mutliply file code

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply{
+    public static void main(String args[]){
+        int a = 5;
+        int b = 10;
+        int multiply = a * b;
+        System.out.println("the multiplication of two numbers is:" + multiply);
+    }
+}
